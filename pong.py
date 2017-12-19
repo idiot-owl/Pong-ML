17b72e78a28ca698487e1f0fafcd967d5039de2f
idiot-owl <hjharshjain1712@gmail.com>
2017-12-20 01:21:19 +0530

    Updated to store training data

--- pong.py
+++ pong.py
@@ -7,7 +7,7 @@ import decimal
 import random
 import scipy as scipy
 from pygame.locals import *
-import pylab as pl
+import pickle
 
 pygame.init()
 
@@ -34,15 +34,23 @@ pygame.draw.rect(DS, WHITE, (0, 10, 15, 100))
 pygame.draw.circle(DS, WHITE,
                    (ballX, int(slope*ballX + intercept)), 10, 1)
 pygame.key.set_repeat(50, 50)
+
+trainX = []
+trainY = []
+X = []
+Y = []
 while True:
+    direction = 0
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
+                direction = 1
                 r1 -= 20
             if event.key == pygame.K_DOWN:
+                direction = 0
                 r1 += 20
             if r1 <= 0:
                 r1 = 0
@@ -83,6 +91,8 @@ while True:
     if ballX < 15:
         if r1 > slope*ballX + intercept or r1 + 100 < slope*ballX + intercept:
             print("Lost")
+            X = []
+            Y = []
             output = 0
             ballX = 15
             slope = 0.3
@@ -96,6 +106,12 @@ while True:
             pygame.display.update()
             pass
         else:
+            print("Hit")
+            trainX += X
+            trainY += Y
+            #print(X)
+            X = []
+            Y = []
             current_y = slope*ballX + intercept
             intercept += 2*slope*ballX
             d = 1
@@ -116,14 +132,17 @@ while True:
         ballX -= 5
     if d == 1:
         ballX += 5
-    X = surfarray.pixels2d(pygame.display.get_surface())
-    X.reshape(250000, 1)
+    X1 = surfarray.pixels2d(pygame.display.get_surface())
+    
     # iska dekh le.. y ka and final trainingset ka jo bhi h
     clock.tick(100)
     DS.fill(BLACK)
     count += 1
-    if(count > 10000):
-        # yaha pr file write kr diyo
+    if(count > 500):
+        with open('trainX.pkl', 'wb') as f:
+            pickle.dump(trainX, f)
+        with open('trainY.pkl', 'wb') as f:
+            pickle.dump(trainY, f)
         pygame.quit()
         sys.exit()
     pygame.draw.rect(DS, WHITE, (0, r1, 15, 100))
@@ -131,3 +150,8 @@ while True:
     pygame.draw.circle(DS, WHITE,
                        (ballX, int(slope*ballX + intercept)), 10)
     pygame.display.update()
+    X += [X1.flatten()]
+    Y += [direction]
+    
+
+
